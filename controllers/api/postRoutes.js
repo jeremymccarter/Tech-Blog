5a4bd06631e955648106ce6ad254f8abea94f17d
Jeremy McCarter <jeremymccarter27@gmail.com>
2023-03-20 16:32:13 -0700

    Finished delete in postRoutes, updated syntax on multiple files, need to finish handlebars layout and test

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -27,6 +27,7 @@ router.post('/', async (req, res) => {
             body: req.body.body,
             user_id: req.session.user_id
         })
+        
         res.status(200).json(newPost);
     }
     catch (err) {
@@ -43,12 +44,33 @@ router.put('/:id', async (req, res) => {
                 where: {
                     id: req.params.id
                 }
-             }
-        )
+             })
+             if (!updatedPost) {
+                res.status(404).json({ message: 'No post found with this id!' });
+                return;
+              }
         res.status(200).json(updatedPost);
     }
     catch (err) {
         res.status(400).json(err)
 }})
 
+router.delete('/:id', async (req, res) => {
+    // delete one product by its `id` value
+    try {
+      const postData = await Post.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      if (!postData) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 module.exports= router
